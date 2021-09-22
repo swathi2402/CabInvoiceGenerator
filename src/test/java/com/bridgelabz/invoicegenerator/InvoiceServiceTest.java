a79bd3351a2b26bfe77fa10cba193c5cea9ec121
Swathi <swathi.h@perfios.com>
2021-09-22 17:14:53 +0530

    ADDED: @Before method added

--- src/test/java/com/bridgelabz/invoicegenerator/InvoiceServiceTest.java
+++ src/test/java/com/bridgelabz/invoicegenerator/InvoiceServiceTest.java
@@ -2,13 +2,20 @@ package com.bridgelabz.invoicegenerator;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class InvoiceServiceTest {
+	
+	InvoiceGenerator invoiceGenerator = null;
+	
+	@Before
+	public void setUp() throws Exception{
+		invoiceGenerator = new InvoiceGenerator();
+	}
 
 	@Test
 	public void givenDistanceAndTime_ShouldReturnTotalFare() {
-		InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
 		double distance = 2.0;
 		int time = 5;
 		double fare = invoiceGenerator.calculateFare(distance, time);
@@ -17,7 +24,6 @@ public class InvoiceServiceTest {
 
 	@Test
 	public void givenMinimumDistance_ShouldReturnMinimumFare() {
-		InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
 		double distance = 0.1;
 		int time = 1;
 		double fare = invoiceGenerator.calculateFare(distance, time);
@@ -26,7 +32,6 @@ public class InvoiceServiceTest {
 
 	@Test
 	public void givenMultipleRides_ShouldReturnInvoiceSummary() {
-		InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
 		Ride[] rides = { new Ride(2.0, 5), new Ride(0.1, 1) };
 		InvoiceSummary fare = invoiceGenerator.calculateFare(rides);
 		InvoiceSummary expected = new InvoiceSummary(2,30);
