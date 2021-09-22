5ba6ec44005dd399739b8637ff23a12e2c4f4825
Swathi <swathi.h@perfios.com>
2021-09-22 16:29:08 +0530

    ADDED: Test to get minimun fare when minimum distance covered

--- src/test/java/com/bridgelabz/invoicegenerator/InvoiceServiceTest.java
+++ src/test/java/com/bridgelabz/invoicegenerator/InvoiceServiceTest.java
@@ -14,4 +14,13 @@ public class InvoiceServiceTest {
 		double fare = invoiceGenerator.calculateFare(distance, time);
 		assertEquals(25, fare, 0.0);
 	}
+	
+	@Test
+	public void givenMinimumDistance_ShouldReturnMinimumFare() {
+		InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+		double distance = 0.1;
+		int time = 1;
+		double fare = invoiceGenerator.calculateFare(distance, time);
+		assertEquals(5, fare, 0.0);
+	}
 }
