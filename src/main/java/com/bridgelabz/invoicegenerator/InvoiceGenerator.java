dd3f6edf00ef4a2b19bf97503f187093e1e94113
Swathi <swathi.h@perfios.com>
2021-09-22 16:30:38 +0530

    MODIFIED: Logic to get minimum fare for minimum distance

--- src/main/java/com/bridgelabz/invoicegenerator/InvoiceGenerator.java
+++ src/main/java/com/bridgelabz/invoicegenerator/InvoiceGenerator.java
@@ -4,9 +4,12 @@ public class InvoiceGenerator {
 
 	private static final double COST_PER_KILOMETER = 10;
 	private static final int COST_PER_TIME = 1;
+	private static final double MINIMUM_FARE = 5;
 
 	public double calculateFare(double distance, int time) {
-		return distance * COST_PER_KILOMETER + time * COST_PER_TIME;
+		double totalFare = distance * COST_PER_KILOMETER + time * COST_PER_TIME;
+
+		return Math.max(totalFare, MINIMUM_FARE);
 	}
 
 }
