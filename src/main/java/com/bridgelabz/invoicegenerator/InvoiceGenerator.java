7c3199ffbf426f03986642255b10dbd5c7fd863d
Swathi <swathi.h@perfios.com>
2021-09-22 19:31:36 +0530

    MODIFIED: Changes made to incorporate ride type

--- src/main/java/com/bridgelabz/invoicegenerator/InvoiceGenerator.java
+++ src/main/java/com/bridgelabz/invoicegenerator/InvoiceGenerator.java
@@ -1,12 +1,29 @@
 package com.bridgelabz.invoicegenerator;
 
+import com.bridgelabz.invoicegenerator.Ride.RideType;
+
 public class InvoiceGenerator {
 
-	private static final double COST_PER_KILOMETER = 10.0;
-	private static final double COST_PER_TIME = 1.0;
-	private static final double MINIMUM_FARE = 5.0;
+	public double calculateFare(double distance, int time, RideType rideType) {
 
-	public double calculateFare(double distance, int time) {
+		double COST_PER_KILOMETER = 0.0;
+		double COST_PER_TIME = 0.0;
+		double MINIMUM_FARE = 0.0;
+
+		switch (rideType) {
+		case NORMAL_RIDE:
+			COST_PER_KILOMETER = 10.0;
+			COST_PER_TIME = 1.0;
+			MINIMUM_FARE = 5.0;
+			break;
+
+		case PREMIUM_RIDE:
+			COST_PER_KILOMETER = 15.0;
+			COST_PER_TIME = 2.0;
+			MINIMUM_FARE = 20.0;
+			break;
+		}
+
 		double totalFare = distance * COST_PER_KILOMETER + time * COST_PER_TIME;
 
 		return Math.max(totalFare, MINIMUM_FARE);
@@ -15,7 +32,7 @@ public class InvoiceGenerator {
 	public InvoiceSummary calculateFare(Ride[] rides) {
 		double totalFare = 0;
 		for (Ride ride : rides) {
-			double fare = this.calculateFare(ride.getDistance(), ride.getTime());
+			double fare = this.calculateFare(ride.getDistance(), ride.getTime(), ride.getRideType());
 			totalFare += fare;
 		}
 		return new InvoiceSummary(rides.length, totalFare);
